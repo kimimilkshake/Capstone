056472fa9a2737eac5b36b70accef997656af4e5
Shem <shemcardoza7@gmail.com>
2025-12-08 23:57:38 +0800

    Minor fixes for admin vessel creation and edit UI

--- storage/framework/views/6e76ce8f098645b7b209ec4daefb6301.php
+++ storage/framework/views/6e76ce8f098645b7b209ec4daefb6301.php
@@ -20,7 +20,6 @@
     <table class="vessel-table">
       <thead>
         <tr>
-          <th>Vessel No.</th>
           <th>Code</th>
           <th>Name</th>
           <th>No. of Hatches</th>
@@ -33,7 +32,6 @@
       <tbody>
         <?php $__empty_1 = true; $__currentLoopData = $vessels; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $index => $v): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); $__empty_1 = false; ?>
           <tr>
-            <td><?php echo e($v->vessel_id); ?></td>
             <td><?php echo e($v->vessel_code); ?></td>
             <td><?php echo e($v->vessel_name); ?></td>
             <td><?php echo e($v->hatches->count()); ?></td>
@@ -48,7 +46,7 @@
           </tr>
         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); if ($__empty_1): ?>
           <tr>
-            <td colspan="7" class="text-center">No vessels found.</td>
+            <td colspan="6" class="text-center">No vessels found.</td>
           </tr>
         <?php endif; ?>
       </tbody>
