74d46cdc16a138ec958784a9b5eaa1a7b82fa585
sophiaacohon <sophiaannu.cohon@gmail.com>
2026-02-24 15:47:07 +0800

    minor edits to auth header and contact us page

--- resources/views/passenger/contact.blade.php
+++ resources/views/passenger/contact.blade.php
@@ -13,9 +13,9 @@
                     <div class="main-office-details">
                         <p><i class="fa-solid fa-building me-3"></i>872 M.J. CUENCO AVENUE, BARANGAY LOREGA,  6000 CEBU CITY, PHILIPPINES</p>
                         <p><i class="fa-solid fa-phone me-3"></i>TEL. #: (032)  232-8864,  232-8865</p>
-                        <p><i class="fa-solid fa-fax me-3"></i>FAX #: (032)  232-8863,  416-7043</p>
+                        <p><i class="fa-solid fa-fax me-3"></i>FAX #: (032)  232-8863</p>
                         <p><i class="fa-solid fa-mobile-screen me-3"></i>CELLPHONE NO.  09091897083</p>
-                        <p><i class="fa-solid fa-envelope me-3"></i>EMAIL: lapulapulslc1964@gmail.com</p>
+                        <p><i class="fa-solid fa-envelope me-3"></i>EMAIL: lapulinesbiz@yahoo.com [TEMPORARY]</p>
                         <p><i class="fa-solid fa-business-time me-3"></i>BUSINESS HOURS: 8AM TO 5PM MONDAY TO SATURDAY</p>
                         <a href="https://www.facebook.com/profile.php?id=100064063478567" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-thumbs-up me-3"></i>Facebook: Lapulapu Shipping Lines Corporation</a>
                     </div>
