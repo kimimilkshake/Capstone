0e8e1f31e975600b2da6cbe4c2885517e2c484c9
sophiaacohon <sophiaannu.cohon@gmail.com>
2026-03-12 02:48:05 +0800

    added questions for FAQs

--- resources/views/passenger/faqs.blade.php
+++ resources/views/passenger/faqs.blade.php
@@ -2,8 +2,42 @@
 @section('content')
     @include('components.hero'){{-- Head Nav --}}
     <div class="faqs-container my-5">
-        <h3 class="faqs-title">Frequently Asked Questions</h3>
+        <h2 class="faqs-title">Frequently Asked Questions</h2>
 
+        <br>
+        <!--GENERAL BOARDING AND LOGISTICS FAQs-->
+        <h4>General Boarding and Logistics</h4>
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>Where are the vessels of Lapulapu Shipping Lines Corp. docked at the Port of Cebu?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>Where is my E-ticket copy?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>My trip has been canceled. Will my ticket be refunded or revalidated?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <br>
+        <!--PASSENGER BOOKING FAQs-->
+        <h4>Passenger Booking</h4>
         <div class="faq-item">
             <button class="faq-question">
                 <span>Are there discounts for passengers who are infants and children?</span>
@@ -27,7 +61,7 @@
 
         <div class="faq-item">
             <button class="faq-question">
-                <span>How far in advance can I book my ticket?</span>
+                <span>What are the requirements for pregnant passengers?</span>
                 <span class="faq-toggle">+</span>
             </button>
             <div class="faq-answer">
@@ -37,7 +71,7 @@
 
         <div class="faq-item">
             <button class="faq-question">
-                <span>How far in advance can I book my ticket?</span>
+                <span>What is the minimum age for travelling alone?</span>
                 <span class="faq-toggle">+</span>
             </button>
             <div class="faq-answer">
@@ -47,7 +81,7 @@
 
         <div class="faq-item">
             <button class="faq-question">
-                <span>How far in advance can I book my ticket?</span>
+                <span>How many days in advance can I purchase my tickets?</span>
                 <span class="faq-toggle">+</span>
             </button>
             <div class="faq-answer">
@@ -57,7 +91,7 @@
 
         <div class="faq-item">
             <button class="faq-question">
-                <span>How far in advance can I book my ticket?</span>
+                <span>Where and how may I ask for a refund of my ticket?</span>
                 <span class="faq-toggle">+</span>
             </button>
             <div class="faq-answer">
@@ -67,7 +101,7 @@
 
         <div class="faq-item">
             <button class="faq-question">
-                <span>How far in advance can I book my ticket?</span>
+                <span>I lost my ticket, can you give me a refund or can you issue another ticket?</span>
                 <span class="faq-toggle">+</span>
             </button>
             <div class="faq-answer">
@@ -75,6 +109,209 @@
             </div>
         </div>
 
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>I want to change my accommodation from Economy to Aircon. How can I upgrade my passenger ticket?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>How many days from sailing date can I ask for a refund or rebooking of my ticket?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>Do you issue an official receipt for the purchase of passenger tickets?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>How long is the validity of my ticket?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>What are the requirements upon boarding the vessel?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>Where and what time should I board the vessel?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>The voyage for a certain sailing date is already fully booked. Can I still board the vessel as a chance passenger?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>Do you provide free meals for the passengers?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>I want to travel with my pet. Is this possible?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <!--CARGO BOOKING FAQs-->
+        <br>
+        <h4>Cargo Booking</h4>
+        
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>Is there insurance for my cargo?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+        
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>Can I book cargo without being a passenger on the same trip?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>What is the "Cut-off Time" for cargo loading?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>How many days in advance can I book for cargo?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>Can I book a Rolling Cargo?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>Can I book cargo for cadaver?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>What are prohibited cargoes on board?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>What documents are required to ship a motorcycle?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>Do I need to drain the fuel before shipping my motorcycle?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>Does the shipping line provide crates for fragile appliances?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
+
+        <div class="faq-item">
+            <button class="faq-question">
+                <span>Can I ship second-hand appliances?</span>
+                <span class="faq-toggle">+</span>
+            </button>
+            <div class="faq-answer">
+                Secret, trip mo lang!
+            </div>
+        </div>
 
     </div>
 @endsection
