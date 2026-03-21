7da9cba82d25a18fca191d2fcfac152706514f09
Clint Laurence Englis <clintenglis16@gmail.com>
2026-03-22 00:12:18 +0800

    toast based error manifest and qr, semaphore popups, loading + success feedbacks, ui improvements, notification dropdown

--- storage/framework/views/b110b6ac734bcb3b5981aaaf7c825c13.php
+++ storage/framework/views/b110b6ac734bcb3b5981aaaf7c825c13.php
@@ -1,6 +1,7 @@
 <?php $__env->startSection('content'); ?>
     <section class="vh-100 d-flex align-items-center justify-content-center login-bg"
         style="background-image: url('<?php echo e(asset('images/login-bg.png')); ?>');">
+
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
@@ -12,21 +13,6 @@
                                     style="height:150px">
                                 <h2 class="fw-bold text-uppercase">WELCOME</h2>
                                 <p class="mb-3">Enter your username and password</p>
-
-                                
-                                <?php if(session('success')): ?>
-                                    <div class="alert alert-success mx-auto" style="text-align: center;">
-                                        <?php echo e(session('success')); ?>
-
-                                    </div>
-                                <?php endif; ?>
-
-                                
-                                <?php if($errors->any()): ?>
-                                    <div class="alert alert-danger mx-auto" style="text-align: center;">
-                                        <div><?php echo e($errors->first()); ?></div>
-                                    </div>
-                                <?php endif; ?>
 
                                 
                                 <form action="<?php echo e(route('login')); ?>" method="POST">
