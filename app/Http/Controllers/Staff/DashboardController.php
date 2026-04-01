55f5a04c52437652fef2b24511c2d7dd0257acc5
sophiaacohon <sophiaannu.cohon@gmail.com>
2026-04-01 14:17:21 +0800

    fixed staff dashboard bug

--- app/Http/Controllers/Staff/DashboardController.php
+++ app/Http/Controllers/Staff/DashboardController.php
@@ -37,11 +37,6 @@ class DashboardController extends Controller
 
         // Passenger bookings confirmed today
         $passengerBookings = PassengerTicket::whereDate('created_at', $today)
-            ->whereIn('booking_ref_no', function($query) {
-                $query->select('booking_ref_no')
-                      ->from('booking')
-                      ->where('booking_status', 'Confirmed');
-            })
             ->distinct('booking_ref_no')
             ->count('booking_ref_no');
 
